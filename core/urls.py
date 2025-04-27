e7f85b65d248c6d604e9a19ef6dbe3834f508c10
Dan <danielkksilva1@gmail.com>
2025-04-27 16:24:55 -0400

    feat: structuring project

---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view())
+]
