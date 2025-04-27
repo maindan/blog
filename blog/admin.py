5628123a806915e5461ea4011bc891d316708ea9
Dan <danielkksilva1@gmail.com>
2025-04-27 17:29:03 -0400

    feat: add tag admin view

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,10 @@ from .models import Post, Tag
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'subtitle', 'created_at')
+    list_display = ('title', 'subtitle', 'created_at',)
     search_fields = ('title', 'created_by',)
     list_filter = ('title',)
+@admin.register(Tag)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
