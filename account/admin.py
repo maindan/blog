887884084e038b660d01656ceefdc2e5537ccf7a
Dan <danielkksilva1@gmail.com>
2025-04-29 18:17:54 -0400

    feat: add admin in django-admin

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ('email', 'username', 'is_staff', 'is_superuser')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Informações pessoais', {'fields': ('first_name', 'last_name')}),
+        ('Permissões', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Datas importantes', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'first_name', 'last_name', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+
+    search_fields = ('email', 'username')
